ae55da5fa2a99c32a74903f91abc5056d429b56d
reeduxx <brighton.ulery@gmail.com>
2025-05-14 00:35:51 -0500

    Testing titlescreen

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,33 @@
-#include "bn_core.h"
-#include "bn_keypad.h"
+#include <bn_core.h>
+#include <bn_cstdlib.h>
+#include <bn_keypad.h>
+#include "bn_regular_bg_items_pokemon_logo.h"
 #include "battle_manager.h"
 #include "rng.h"
 
+void titlescreen() {
+	bn::regular_bg_ptr bg = bn::regular_bg_items::pokemon_logo.create_bg(0, 0);
+	
+	while(!bn::keypad::start_pressed()) {
+		bn::core::update();
+	}
+
+	bg.set_visible(false);
+}
+
 int main() {
     bn::core::init();
-	BattleManager battle_manager;
 	int seed = 0;
-	
+	titlescreen();
+
 	while(!bn::keypad::start_pressed()) {
 		++seed;
 		bn::core::update();
 	}
 
 	RNG::instance().seed(0x41C64E6D * seed + 0x0000303B);
-	
+	BattleManager battle_manager;
+
     while(true) {
 		battle_manager.update();
 		RNG::instance().update();
